b648eef5d3b31c4067985ad14cde37c91dae53db
Matt Allwright <matt.a.4890@gmail.com>
2018-12-31 17:39:46 -0700

    Added comments and made some basic optimization changes.

--- Src/ReadBarometer.c
+++ Src/ReadBarometer.c
@@ -1,3 +1,12 @@
+/**
+ * This file contains constants and functions designed to obtain accurate pressure
+ * and temperature readings from the MS5607-02BA03 barometer on the board.
+ */
+
+//
+// Global Headers
+//
+
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_conf.h"
 #include "cmsis_os.h"
@@ -6,127 +15,236 @@
 #include "ReadBarometer.h"
 #include "Data.h"
 
+//
+// Local Headers
+//
+
+uint16_t readCalibrationCoefficient(const uint8_t PROM_READ_CMD);
+
+//
+// Constants
+//
+
 static const int READ_BAROMETER_PERIOD = 20;    // Sampling delay set to 50 Hz to match high frequency logging
 
 static const int CMD_SIZE = 1;
 static const int CMD_TIMEOUT = 150;
 
-static const uint8_t ADC_D1_512_CONV_CMD = 0x42;
-static const uint8_t ADC_D2_512_CONV_CMD = 0x52;
-static const uint8_t ADC_READ_CMD = 0x00;
-static const uint8_t PROM_READ_SENS_CMD = 0xA2;
-static const uint8_t PROM_READ_OFF_CMD = 0xA4;
-static const uint8_t PROM_READ_TCS_CMD = 0xA6;
-static const uint8_t PROM_READ_TCO_CMD = 0xA8;
-static const uint8_t PROM_READ_TREF_CMD = 0xAA;
+static const uint8_t ADC_D1_512_CONV_CMD	= 0x42;
+static const uint8_t ADC_D2_512_CONV_CMD	= 0x52;
+static const uint8_t ADC_READ_CMD			= 0x00;
+static const uint8_t PROM_READ_SENS_CMD		= 0xA2;
+static const uint8_t PROM_READ_OFF_CMD		= 0xA4;
+static const uint8_t PROM_READ_TCS_CMD		= 0xA6;
+static const uint8_t PROM_READ_TCO_CMD		= 0xA8;
+static const uint8_t PROM_READ_TREF_CMD		= 0xAA;
 static const uint8_t PROM_READ_TEMPSENS_CMD = 0xAC;
-static const uint8_t RESET_CMD = 0x1E;
+static const uint8_t READ_BYTE_CMD			= 0x00;
+static const uint8_t RESET_CMD 				= 0x1E;
 
-uint16_t readCalibrationCoefficient(const uint8_t PROM_READ_CMD);
-
+/**
+ * This function is to be used as a thread task that reads and updates
+ * pressure and temperature readings from the barometer.
+ */
 void readBarometerTask(void const* arg)
 {
-    BarometerData* data = (BarometerData*) arg;
-    uint32_t prevWakeTime = osKernelSysTick();
+	/**
+	 * Variable Descriptions from MS5607-02BA03 Data Sheet:
+	 *
+	 * C1 (SENSt1)		- Pressure sensitivity
+	 * C2 (OFFt1)		- Pressure offset
+	 * C3 (TCS)			- Temperature coefficient of pressure sensitivity
+	 * C4 (TCO)			- Temperature coefficient of pressure offset
+	 * C5 (Tref)		- Reference temperature
+	 * C6 (TEMPSENS)	- Temperature coefficient of the temperature
+	 *
+	 * D1	- Digital pressure value
+	 * D2	- Digital temperature value
+	 *
+	 * dT	- Difference between actual and reference temperature
+	 * 			dT = D2 - Tref = D2 - (C5 * 2^8)
+	 * TEMP	- Actual temperature (-40...85°C with 0.01°C resolution)
+	 * 			TEMP = 20°C + (dT * TEMPSENS) = 2000 + (dT * C6)/2^23
+	 * OFF	- Offset at actual temperature
+	 * 			OFF = OFFt1 + (TCO * dT) = (C2 * 2^17) + (C4 * dT)/2^6
+	 * SENS	- Sensitivity at actual temperature
+	 * 			SENS = SENSt1 + (TCS * dT) = (C1 * 2^16) + (C3 * dT)/2^7
+	 * P	- Temperature compensated pressure (10...1200mbar with 0.01mbar resolution)
+	 * 			P = (D1 * SENS) - OFF = ((D1 * SENS)/2^21 - OFF)/2^15
+	 */
 
+	// Variables
+    BarometerData* data			= (BarometerData*) arg;
+    uint32_t prevWakeTime		= osKernelSysTick();
+	uint32_t pressureReading	= 0;   	// Stores a 24 bit value
+	uint32_t temperatureReading	= 0;	// Stores a 24 bit value
+	uint8_t dataInBuffer;
+
+	// Reset the barometer
     HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(&hspi2, &RESET_CMD, CMD_SIZE, CMD_TIMEOUT);
-    osDelay(3);   // 2.8ms reload after Reset command
+    osDelay(3);	// 2.8ms reload after Reset command
     HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
 
-    uint8_t dataInBuffer;
+    // Read PROM for calibration offsets
+    uint16_t c1Sens		= readCalibrationCoefficient(PROM_READ_SENS_CMD);
+    uint16_t c2Off		= readCalibrationCoefficient(PROM_READ_OFF_CMD);
+    uint16_t c3Tcs		= readCalibrationCoefficient(PROM_READ_TCS_CMD);
+    uint16_t c4Tco		= readCalibrationCoefficient(PROM_READ_TCO_CMD);
+    uint16_t c5Tref		= readCalibrationCoefficient(PROM_READ_TREF_CMD);
+    uint16_t c6Tempsens	= readCalibrationCoefficient(PROM_READ_TEMPSENS_CMD);
 
-    // PROM read for calibration coefficients
-    uint16_t c1Sens = readCalibrationCoefficient(PROM_READ_SENS_CMD);
-    uint16_t c2Off = readCalibrationCoefficient(PROM_READ_OFF_CMD);
-    uint16_t c3Tcs = readCalibrationCoefficient(PROM_READ_TCS_CMD);
-    uint16_t c4Tco = readCalibrationCoefficient(PROM_READ_TCO_CMD);
-    uint16_t c5Tref = readCalibrationCoefficient(PROM_READ_TREF_CMD);
-    uint16_t c6Tempsens = readCalibrationCoefficient(PROM_READ_TEMPSENS_CMD);
-
-    uint32_t pressureReading = 0;   // Stores a 24 bit value
-    uint32_t temperatureReading = 0;   // Stores a 24 bit value
-
-    for (;;)
+	/**
+	 * Repeatedly read digital pressure and temperature.
+	 * Convert these values into their calibrated counterparts.
+	 * Finally, update the data globally if the mutex is available.
+	 *
+	 * Restricted to 50Hz.
+	 */
+    while(1)
     {
+		// Delay so that the loop operates at 50Hz
         osDelayUntil(&prevWakeTime, READ_BAROMETER_PERIOD);
 
-        // Read D1 pressure
+		//
+        // Read Digital Pressure (D1)
+		//
+
+		// Tell the barometer to convert the pressure to a digital value with an OSR of 512
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
         HAL_SPI_Transmit(&hspi2, &ADC_D1_512_CONV_CMD, CMD_SIZE, CMD_TIMEOUT);
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
 
         osDelay(2); // 1.17ms max conversion time for OSR 512
 
+		// Read the pressure value
+
+		// Chip select to start a new command
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
+
+		// Tell the chip we want to read from the adc
         HAL_SPI_Transmit(&hspi2, &ADC_READ_CMD, CMD_SIZE, CMD_TIMEOUT);
 
-        pressureReading = 0;
+		// Read the first byte (bits 23-16)
+        HAL_SPI_TransmitReceive(&hspi2, &READ_BYTE_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
+        pressureReading = dataInBuffer << 16;
 
-        HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
-        pressureReading += dataInBuffer << 16;
-
-        HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
+		// Read the second byte (bits 15-8)
+        HAL_SPI_TransmitReceive(&hspi2, &READ_BYTE_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
         pressureReading += dataInBuffer << 8;
 
-        HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
+		// Read the third byte (bits 7-0)
+        HAL_SPI_TransmitReceive(&hspi2, &READ_BYTE_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
         pressureReading += dataInBuffer;
 
+		// Release chip select to end the command
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
 
-        // Read D2 temperature
+		//
+		// Read Digital Temperature (D2)
+		//
+
+		// Tell the barometer to convert the temperature to a digital value with an OSR of 512
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
         HAL_SPI_Transmit(&hspi2, &ADC_D2_512_CONV_CMD, CMD_SIZE, CMD_TIMEOUT);
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
 
         osDelay(2); // 1.17ms max conversion time for OSR 512
 
+		// Read the temperature value
+
+		// Chip select to start a new command
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
+
+		// Tell the chip we want to read from the adc
         HAL_SPI_Transmit(&hspi2, &ADC_READ_CMD, CMD_SIZE, CMD_TIMEOUT);
 
-        temperatureReading = 0;
-
+		// Read the first byte (bits 23-16)
         HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
-        temperatureReading += dataInBuffer << 16;
+        temperatureReading = dataInBuffer << 16;
 
+		// Read the second byte (bits 15-8)
         HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
         temperatureReading += dataInBuffer << 8;
 
+		// Read the third byte (bits 7-0)
         HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
         temperatureReading += dataInBuffer;
 
+		// Release chip select to end the command
         HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
 
-        // calculate 1st order pressure and temperature (1st order algorithm from MS5607-02BA03 datasheet)
-        double dT = temperatureReading - c5Tref * pow(2, 8);
-        double temp = 2000 + dT * c6Tempsens / pow(2, 23);
-        double off = c2Off * pow(2, 17) + dT * c4Tco / pow(2, 6);
-        double sens = c1Sens * pow(2, 16) + dT * c3Tcs / pow(2, 7);
-        // pressure and temperature were not divided by 100 to keep the decimal places
-        double pressure = (pressureReading * sens / pow(2, 21) - off) / pow(2, 15); // need to divide P by 100 to get mbar
-        double temperature = (temp); // need to divide T by 100 to get degrees Celcius
+		//
+		// Calculate First-Order Temperature and Pressure
+		//
 
+		// Old code (Haven't deleted just in case new code doesn't work (something something version control)... although it should work)
+		/*
+        double dT	= temperatureReading - c5Tref * pow(2, 8);
+        double temp	= 2000 + dT * c6Tempsens / pow(2, 23);
+        double off	= c2Off * pow(2, 17) + dT * c4Tco / pow(2, 6);
+        double sens	= c1Sens * pow(2, 16) + dT * c3Tcs / pow(2, 7);
+
+		// PRESSURE AND TEMPERATURE WERE NOT DIVIDED BY 100 IN ORDER TO KEEP DECIMAL PLACES - IN ACCORDANCE WITH THE DATA SHEET \\
+
+        double pressure		= (pressureReading * sens / pow(2, 21) - off) / pow(2, 15); // need to divide P by 100 to get mbar
+        double temperature	= (temp); // need to divide T by 100 to get degrees Celcius
+		*/
+
+		// New code
+		int32_t dT		= temperatureReading - (c5Tref << 8);
+        int32_t temp	= 2000 + ((dT * c6Tempsens) >> 23);
+        int32_t off		= (c2Off << 17) + ((dT * c4Tco) >> 6);
+        int32_t sens	= (c1Sens << 16) + ((dT * c3Tcs) >> 7);
+        int32_t p		= ((pressureReading * sens) >> 21) - off) >> 15;
+
+		// Check if the mutex is available
         if (osMutexWait(data->mutex_, 0) != osOK)
         {
+			// Loop and get new values if the mutex was taken
             continue;
         }
 
-        data->pressure_ = (int32_t) pressure;
-        data->temperature_ = (int32_t) temperature;
+		// Assign new values if mutex was available
+        data->pressure_		= p;
+        data->temperature_	= temp;
         osMutexRelease(data->mutex_);
     }
 }
 
+/**
+ * This function reads and returns a 16-bit coefficient from the barometer.
+ * @param	PROM_READ_CMD	The command to send in order to read the desired coefficient.
+ * 							See the data sheet for the commands.
+ * @return					The read coefficient.
+ */
 uint16_t readCalibrationCoefficient(const uint8_t PROM_READ_CMD)
 {
-    uint8_t dataInBuffer;
-    uint16_t coefficient = 0;
+	// Variables
+    uint16_t	coefficient;
+    uint8_t		dataInBuffer;
+
+	//
+	// Read Coefficient
+	//
+
+	// Chip select to start a new command
     HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_RESET);
+
+	// Tell the chip we want to read from PROM
     HAL_SPI_Transmit(&hspi2, &PROM_READ_CMD, CMD_SIZE, CMD_TIMEOUT);
-    HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
+
+	// Read the first byte (bits 15-8)
+    HAL_SPI_TransmitReceive(&hspi2, &READ_BYTE_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
     coefficient = dataInBuffer << 8;
-    HAL_SPI_TransmitReceive(&hspi2, &ADC_READ_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
+
+	// Read the second byte (bits 7-0)
+    HAL_SPI_TransmitReceive(&hspi2, &READ_BYTE_CMD, &dataInBuffer, CMD_SIZE, CMD_TIMEOUT);
     coefficient += dataInBuffer;
+
+	// Release chip select to end the command
     HAL_GPIO_WritePin(BARO_CS_GPIO_Port, BARO_CS_Pin, GPIO_PIN_SET);
+
+	// Return the read coefficient
     return coefficient;
 }
