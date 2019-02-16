90b3e9dc309fbc0fa813b906e7f827a07a00840f
Matt Allwright <matt.a.4890@gmail.com>
2019-02-16 12:14:20 -0700

    Commented ParachutesControl.c

--- Src/ParachutesControl.c
+++ Src/ParachutesControl.c
@@ -1,3 +1,16 @@
+/**
+  ******************************************************************************
+  * File Name          : ReadBarometer.c
+  * Description        : This file contains constants and functions designed to
+  *                      obtain accurate pressure and temperature readings from
+  *                      the MS5607-02BA03 barometer on the flight board. A
+  *                      thread task is included that will constantly loop,
+  *                      reading and updating the passed BarometerData struct.
+  ******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+
 #include <math.h>
 
 #include "stm32f4xx.h"
@@ -9,9 +22,21 @@
 #include "Data.h"
 #include "AltitudeKalmanFilter.h"
 
+/* Macros --------------------------------------------------------------------*/
+
+/* Constants -----------------------------------------------------------------*/
+
 // Pressure at spaceport america in 100*millibars on May 27, 2018
 static const int MAIN_DEPLOYMENT_ALTITUDE = 457 + 1401; // Units in meters. Equivalent of 15000 ft + altitude of spaceport america.
 static const int MONITOR_FOR_PARACHUTES_PERIOD = 200;
+
+/* Variables -----------------------------------------------------------------*/
+
+/* Structs -------------------------------------------------------------------*/
+
+/* Prototypes ----------------------------------------------------------------*/
+
+/* Functions -----------------------------------------------------------------*/
 
 /**
  * Takes the current state vector and determines if apogee has been reached.
@@ -56,18 +81,24 @@ int32_t detectMainDeploymentAltitude(struct KalmanStateVector state)
     return 0;
 }
 
+/**
+ * Releases the drouge parachute on the rocket.
+ */
 void ejectDrogueParachute()
 {
     HAL_GPIO_WritePin(DROGUE_PARACHUTE_GPIO_Port, DROGUE_PARACHUTE_Pin, GPIO_PIN_SET);  // high signal causes high current to ignite e-match
 }
 
+/**
+ * Releases the main parachute on the rocket.
+ */
 void ejectMainParachute()
 {
     HAL_GPIO_WritePin(MAIN_PARACHUTE_GPIO_Port, MAIN_PARACHUTE_Pin, GPIO_PIN_SET);  // high signal causes high current to ignite e-match
 }
 
 /**
- * This routine just waits for the current flight phase to get out of PRELAUNCH
+ * Waits for the current flight phase to get out of PRELAUNCH
  */
 void parachutesControlPrelaunchRoutine()
 {
@@ -85,6 +116,14 @@ void parachutesControlPrelaunchRoutine()
     }
 }
 
+/**
+ * Desc
+ *
+ * Params:
+ *   accelGyromMagnetismData	- (AccelGyroMagnetismData*) Pointer to the IMU data struct to read the acceleration from.
+ *   barometerData				- (barometerData*) Pointer to the barometer data struct to read the pressure from.
+ *   state						- (KalmanStateVector) The current acceleration, pressure, and altitude state of the rocket.
+ */
 void parachutesControlBurnRoutine(
     AccelGyroMagnetismData* accelGyroMagnetismData,
     BarometerData* barometerData,
@@ -117,9 +156,13 @@ void parachutesControlBurnRoutine(
 
 
 /**
- * This routine monitors for apogee.
- * Once apogee has been detected,
+ * Monitors for apogee. Once apogee has been detected,
  * eject the drogue parachute and update the current flight phase.
+ *
+ * Params:
+ *   accelGyromMagnetismData	- (AccelGyroMagnetismData*) Pointer to the IMU data struct to read the acceleration from.
+ *   barometerData				- (barometerData*) Pointer to the barometer data struct to read the pressure from.
+ *   state						- (KalmanStateVector) The current acceleration, pressure, and altitude state of the rocket.
  */
 void parachutesControlCoastRoutine(
     AccelGyroMagnetismData* accelGyroMagnetismData,
@@ -154,9 +197,14 @@ void parachutesControlCoastRoutine(
 }
 
 /**
- * This routine detects reaching a certain altitude
+ * This routine detects reaching a certain altitude.
  * Once that altitude has been reached, eject the main parachute
  * and update the current flight phase.
+ *
+ * Params:
+ *   accelGyromMagnetismData	- (AccelGyroMagnetismData*) Pointer to the IMU data struct to read the acceleration from.
+ *   barometerData				- (barometerData*) Pointer to the barometer data struct to read the pressure from.
+ *   state						- (KalmanStateVector) The current acceleration, pressure, and altitude state of the rocket.
  */
 void parachutesControlDrogueDescentRoutine(
     AccelGyroMagnetismData* accelGyroMagnetismData,
@@ -192,6 +240,15 @@ void parachutesControlDrogueDescentRoutine(
     }
 }
 
+/**
+ * This function is to be used as a thread task that reads and updates
+ * a kalman state vector, which it uses to detect the apogee of the rocket's
+ * flight path. Once apogee is detected, a drouge parachute will be released.
+ * Some time after this, the main parachute will also be deployed.
+ *
+ * Params:
+ *   arg - (void const*) A pointer to the ParachutesControlData struct that will be used.
+ */
 void parachutesControlTask(void const* arg)
 {
     ParachutesControlData* data = (ParachutesControlData*) arg;
