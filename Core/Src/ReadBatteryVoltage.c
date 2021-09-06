e093ae4a3454b8a38f2d2d1d8077b15e025c4540
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-09-05 18:13:26 -0600

    Setup the .h file and update the includes for the ReadBatteryVoltage file

---
+++ Core/Src/ReadBatteryVoltage.c
@@ -0,0 +1,11 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_conf.h"
+#include "cmsis_os.h"
+
+#include "ReadBatteryVoltage.h"
+
+#include "Data.h"
+
+void readBatteryVoltageTask(void const* arg) {
+    
+}
