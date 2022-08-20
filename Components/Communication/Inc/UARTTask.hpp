b983ffaa66dcb17825ab714cd2de699b71fcf432
Chris <78698227+cjchanx@users.noreply.github.com>
2022-08-19 23:21:49 -0600

    Begin implementation of UARTTask::Init

--- Components/Communication/Inc/UARTTask.hpp
+++ Components/Communication/Inc/UARTTask.hpp
@@ -6,13 +6,45 @@
 */
 #ifndef SOAR_COMMS_UARTTASK_HPP_
 #define SOAR_COMMS_UARTTASK_HPP_
+/* Includes ------------------------------------------------------------------*/
 #include "Task.hpp"
 #include "SystemDefines.hpp"
 
+
+
+/* Macros ------------------------------------------------------------------*/
+
+
+
+/* Class ------------------------------------------------------------------*/
 class UARTTask : Task
 {
-	// Things go here!
+public:
+	static UARTTask& Inst() {
+		static UARTTask inst;
+		return inst;
+	}
+
+	void InitTask();
+
+protected:
+	static void Run(void* pvParams); // Task loop
+
+
+private:
+	UARTTask() : Task(FLIGHT_TASK_QUEUE_SIZE) {}	// Private constructor
+	UARTTask(const UARTTask&);						// Prevent copy-construction
+	UARTTask& operator=(const UARTTask&);			// Prevent assignment
+
+
 };
+
+
+/* Utility Functions ------------------------------------------------------------------*/
+namespace UARTUtils
+{
+	
+}
 
 
 #endif	// SOAR_COMMS_UARTTASK_HPP_
