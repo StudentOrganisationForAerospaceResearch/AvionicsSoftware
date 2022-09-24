b13c98cbe5155b93cf7048a67af0f0bad7b4d8ef
cjchanx <cjchan.chris@gmail.com>
2022-09-24 15:43:40 -0600

    Update .ioc to Andromeda V3.3

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -52,10 +52,10 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
-void DMA1_Stream0_IRQHandler(void);
+void DMA1_Stream2_IRQHandler(void);
 void TIM1_UP_TIM10_IRQHandler(void);
 void USART2_IRQHandler(void);
-void DMA1_Stream7_IRQHandler(void);
+void UART5_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
