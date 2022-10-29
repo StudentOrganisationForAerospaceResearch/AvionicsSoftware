09aeb32eb9b0ccad0d441bebb8310f26c248b801
Chris <78698227+cjchanx@users.noreply.github.com>
2022-10-28 21:24:01 -0600

    Code cleanup

--- Core/Startup/startup_stm32f405rgtx.s
+++ Core/Startup/startup_stm32f405rgtx.s
@@ -59,7 +59,7 @@ defined in linker script */
   .weak  Reset_Handler
   .type  Reset_Handler, %function
 Reset_Handler:  
-   ldr   sp, =_estack     /* set stack pointer */
+  ldr   sp, =_estack     /* set stack pointer */
 
 /* Copy the data segment initializers from flash to SRAM */  
   ldr r0, =_sdata
