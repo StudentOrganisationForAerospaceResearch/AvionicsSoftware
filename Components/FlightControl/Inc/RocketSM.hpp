fc073247dc75e640adc49a267d71bef0a6022cd9
cjchanx <cjchan.chris@gmail.com>
2022-11-12 16:11:47 -0700

    Begin Implementation of Rocket State Machine

---
+++ Components/FlightControl/Inc/RocketSM.hpp
@@ -0,0 +1,80 @@
+/**
+ ******************************************************************************
+ * File Name          : RocketSM.hpp
+ * Description        : Rocket state machine, handles all rocket state transitions.
+ ******************************************************************************
+*/
+#ifndef SOAR_AVIONICS_ROCKET_SM
+#define SOAR_AVIONICS_ROCKET_SM
+
+#include "Command.hpp"
+
+enum RocketState
+{
+	RS_PRELAUNCH = 0,	// Idle state, waiting for command to proceeding sequences
+	RS_PREFILL,		// Pressure testing of N2 (?) and LOX
+	RS_FILL,		// Control of solenoid valves
+	RS_ARM,			// We don't allow fill etc. 1-2 minutes before launch
+	RS_IGNITION,	// Ignition of the ignitors
+	RS_LAUNCH,		// Launch triggered by confirmation of ignition (from ignitor) is nominal
+	RS_BURN,		// Main burn (vents closed MEV open) - 5-6 seconds (TBD)
+	RS_COAST,		// Coasting (MEV closed, vents closed) - 30 seconds (TBD)
+	RS_POSTAPOGEE,	// Vents open (well into the descent)
+	RS_ABORT,		// Abort sequence, vents open, MEV closed, ignitors off
+	RS_NONE			// Invalid state, must be last
+};
+
+/**
+ * @brief Base class for Rocket State Machine
+ */
+class BaseRocketState
+{
+public:
+	virtual void HandleCommand(Command& cm) = 0; //Handle a command based on the current state
+	virtual RocketState OnEnter() = 0;	//Returns the state we're entering
+	virtual RocketState OnExit() = 0;	//Returns the state we're exiting
+
+	RocketState GetStateName() { return stateName; }
+protected:
+	RocketState stateName = RS_NONE;	//The name of the state we're in
+
+};
+
+/**
+ * @brief Rocket State Machine
+ */
+class RocketControl
+{
+public:
+	RocketControl();
+
+	void HandleCommand(Command& cm);
+
+protected:
+	RocketState TransitionState(RocketState	nextState);
+
+	// Variables
+	BaseRocketState* stateArray[RS_NONE-1];
+	BaseRocketState* rs_currentState;
+};
+
+/**
+ * @brief PreLaunch state, waiting for commands to proceeding sequences
+ */
+class PreLaunch : public BaseRocketState
+{
+public:
+	PreLaunch();
+
+	void HandleCommand(Command& cm);
+	RocketState OnEnter();
+	RocketState OnExit();
+};
+
+
+
+
+
+
+
+#endif // SOAR_AVIONICS_ROCKET_SM
---
+++ Components/FlightControl/RocketSM.cpp
@@ -0,0 +1,77 @@
+/**
+ ******************************************************************************
+ * File Name          : RocketSM.cpp
+ * Description        : Primary rocket state machine
+ ******************************************************************************
+*/
+#include "RocketSM.hpp"
+#include "SystemDefines.hpp"
+/* Rocket Control ------------------------------------------------------------------*/
+/**
+ * @brief Default constructor for Rocket SM, initializes all states
+ */
+RocketControl::RocketControl()
+{
+	// Setup the internal array of states
+	stateArray[RS_PRELAUNCH] = new PreLaunch();
+
+	// Verify all states are initialized
+	for (uint8_t i = 0; i < RS_NONE-1; i++)
+		SOAR_ASSERT(stateArray[i] != nullptr);
+}
+
+/**
+ * @brief Handles state transitions
+ * @param nextState The next state to transition to
+ * @return The state after the transition
+ */
+RocketState RocketControl::TransitionState(RocketState nextState)
+{
+	// Exit the current state
+	rs_currentState->OnExit();
+
+	// Set the next state
+	rs_currentState = stateArray[nextState];
+
+	// Enter the current state
+	rs_currentState->OnEnter();
+
+	// Return the state after the transition
+	return rs_currentState->GetStateName();
+}
+
+
+/* PreLaunch State ------------------------------------------------------------------*/
+/**
+ * @brief PreLaunch state constructor
+ */
+PreLaunch::PreLaunch()
+{
+	stateName = RS_PRELAUNCH;
+}
+
+/**
+ * @brief Entry to PreLaunch state
+ * @return The state we're entering
+ */
+RocketState PreLaunch::OnEnter()
+{
+	// We don't do anything upon entering prelaunch
+
+	return stateName;
+}
+
+/**
+ * @brief Exit from PreLaunch state
+ * @return The state we're exiting
+ */
+RocketState PreLaunch::OnExit()
+{
+	// We don't do anything upon exiting prelaunch
+
+	return stateName;
+}
+
+
+/* Some State ------------------------------------------------------------------*/
+
