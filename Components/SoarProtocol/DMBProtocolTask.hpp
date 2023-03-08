f6ba5f81819682119335659649e17ad02c97db1d
Christopher Chan <cjchan.chris@gmail.com>
2023-03-07 19:03:01 -0700

    Fix class definition

--- Components/SoarProtocol/DMBProtocolTask.hpp
+++ Components/SoarProtocol/DMBProtocolTask.hpp
@@ -24,29 +24,15 @@ public:
 
     void InitTask();
 
-    //Functions exposed to HAL callbacks
-    void InterruptRxData();
-
 protected:
-    // NOTE: This must be in the derived class
     static void RunTask(void* pvParams) { DMBProtocolTask::Inst().Run(pvParams); } // Static Task Interface, passes control to the instance Run();
-    void Run(void* pvParams);    // Main run code
 
-    void ConfigureUART();
     // This will receive a (DMB_PROTO_COMMAND, DMB_PROTO_RX_DECODED_DATA) with the data pointer allocated, COBS decoded (but in the SOAR Message Format)
     void HandleProtocolMessage(Command& cmd) override;
-    //void HandleCommand(Command& cm);
-
-    bool ReceiveData();
 
     // Helper functions
 
     // Member variables
-    uint8_t* protocolRxBuffer;
-    uint16_t protocolMsgIdx;
-    bool isDMBProtocolMsgReady;
-
-    uint8_t protocolRxChar; // Character received from UART Interrupt
 
 private:
     DMBProtocolTask();        // Private constructor
