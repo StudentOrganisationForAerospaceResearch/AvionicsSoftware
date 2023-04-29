70e231924b3318f0562baa0e9055431555512862
Matthew Allwright <matt.a.4890@gmail.com>
2023-04-29 11:52:29 -0600

    Make docker build more efficient

--- Dockerfile
+++ Dockerfile
@@ -7,19 +7,19 @@ ARG CMAKE_OPTIONS=
 ENV DEBIAN_FRONTEND=noninteractive
 RUN env \
   && apt-get update \
-  && apt-get install -q -y git cmake make g++ lcov gettext-base jq curl \
+  && apt-get install -q -y git cmake make g++ lcov gettext-base jq curl expect-dev \
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/*
 
 # Compilation
 
-ENV BUILD_DIR=build
+# ENV BUILD_DIR=build
 
-ADD . /repo
-WORKDIR /repo
-RUN cmake -S . -B $BUILD_DIR
-RUN cmake --build $BUILD_DIR
+# ADD . /repo
+# WORKDIR /repo
+# RUN cmake -S . -B $BUILD_DIR
+# RUN cmake --build $BUILD_DIR
 
 # Run
 
-RUN cd $BUILD_DIR && ctest
+# RUN cd $BUILD_DIR && ctest
--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
-docker build -t gtest .
+#DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker build -t gtest . #--build-arg CACHEBUST=$(date +%s) # --no-cache
+# DOCKER_BUILDKIT=1 docker build -t gtest . #--build-arg CACHEBUST=$(date +%s) # --no-cache
 # docker run --rm -it --mount type=bind,source="$(pwd)",target=/repo --entrypoint bash gtest
-docker run --rm -it --entrypoint bash gtest
+# docker run --rm -it --entrypoint bash gtest
+
+LOG_FILE=logfile.txt
+
+DOCKER_BUILDKIT=1 docker build -t gtest .
+docker run --rm --mount type=bind,source="$(pwd)",target=/repo -t gtest \
+  bash -c "cd /repo && cmake -S . -B build && cmake --build build && unbuffer build/tests | tee $LOG_FILE"
+
+sed -i -e "s/\x1B[^m]*m//g" $LOG_FILE
