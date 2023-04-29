1f6a3a96db79e80a647f8ed20d91a8a6287a8f52
Matthew Allwright <matt.a.4890@gmail.com>
2023-04-29 12:18:35 -0600

    Add timestamped logs

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
-#DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker build -t gtest . #--build-arg CACHEBUST=$(date +%s) # --no-cache
-# DOCKER_BUILDKIT=1 docker build -t gtest . #--build-arg CACHEBUST=$(date +%s) # --no-cache
-# docker run --rm -it --mount type=bind,source="$(pwd)",target=/repo --entrypoint bash gtest
-# docker run --rm -it --entrypoint bash gtest
+LOG_DIR=./logs
+LOG_FILE=log_$(date +%F_%H:%M:%S).txt
 
-LOG_FILE=logfile.txt
+if [ ! -d './logs' ]; then
+  mkdir ./logs
+fi
 
 DOCKER_BUILDKIT=1 docker build -t gtest .
 docker run --rm --mount type=bind,source="$(pwd)",target=/repo -t gtest \
-  bash -c "cd /repo && cmake -S . -B build && cmake --build build && unbuffer build/tests | tee $LOG_FILE"
+  bash -c "cd /repo && \
+           cmake -S . -B build && \
+           cmake --build build && \
+           unbuffer build/tests | tee $LOG_DIR/$LOG_FILE"
 
-sed -i -e "s/\x1B[^m]*m//g" $LOG_FILE
+sed -i -e 's/\x1B[^m]*m//g' "$LOG_DIR/$LOG_FILE"
+cp "$LOG_DIR/$LOG_FILE" "$LOG_DIR/log_latest.txt"
