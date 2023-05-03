00bf571e361c28d79c87a6ecca32013d09229885
Christopher Chan <cjchan.chris@gmail.com>
2023-05-02 22:43:27 -0600

    Tabs->Spaces

--- Components/Sensors/GPSTask.cpp
+++ Components/Sensors/GPSTask.cpp
@@ -152,7 +152,7 @@ void GPSTask::HandleCommand(Command& cm)
     }
     case DATA_COMMAND: {
         if (cm.GetTaskCommand() == EVENT_GPS_RX_PARSE_READY)
-			ParseGpsData();
+            ParseGpsData();
         break;
     }
     default:
@@ -214,121 +214,121 @@ void GPSTask::ParseGpsData()
         SOAR_PRINT("GPS Warning - Could not acquire mutex for parsing!\n");
     }
 
-	// Vars
-	char* gps_item = &data->buffer_[0];
-	uint8_t item_len = 0;
-	uint8_t counter = 0;
-	uint8_t done = 0;
-	char direction;
+    // Vars
+    char* gps_item = &data->buffer_[0];
+    uint8_t item_len = 0;
+    uint8_t counter = 0;
+    uint8_t done = 0;
+    char direction;
 
-	// Keeps parsing tokens while one of the delimiters present in gps_item
-	do
-	{
-		for (uint8_t i = 0; i < NMEA_MAX_LENGTH + 1; i++) {
-			if (gps_item[i] == ',') {
-				gps_item[i] = 0;
-				item_len = i;
-				break;
-			}
-			else if (gps_item[i] == 0) {
-				done = 1;
-				break;
-			}
-		}
+    // Keeps parsing tokens while one of the delimiters present in gps_item
+    do
+    {
+        for (uint8_t i = 0; i < NMEA_MAX_LENGTH + 1; i++) {
+            if (gps_item[i] == ',') {
+                gps_item[i] = 0;
+                item_len = i;
+                break;
+            }
+            else if (gps_item[i] == 0) {
+                done = 1;
+                break;
+            }
+        }
 
-		if (gps_item[0] != 0) {
-			switch (counter)
-			{
-				// case 0 is when gps_item is "$GPGGA"
-			case 1:
-			{
-				data->time_ = (uint32_t)(atof(gps_item) * 100); // HHMMSS.SS format. Time is multiplied by 100.
-				break;
-			}
+        if (gps_item[0] != 0) {
+            switch (counter)
+            {
+                // case 0 is when gps_item is "$GPGGA"
+            case 1:
+            {
+                data->time_ = (uint32_t)(atof(gps_item) * 100); // HHMMSS.SS format. Time is multiplied by 100.
+                break;
+            }
 
-			case 2:
-			{
-				double latitude = (atof(gps_item)); // DDMM.MMMMMM
-				data->latitude_.degrees_ = (int32_t)latitude / 100; // First 2 numbers are the latitude degrees
-				data->latitude_.minutes_ = (int32_t)((latitude - data->latitude_.degrees_ * 100) * 100000); // Latitude minutes is multplied by 100000
-				break;
-			}
+            case 2:
+            {
+                double latitude = (atof(gps_item)); // DDMM.MMMMMM
+                data->latitude_.degrees_ = (int32_t)latitude / 100; // First 2 numbers are the latitude degrees
+                data->latitude_.minutes_ = (int32_t)((latitude - data->latitude_.degrees_ * 100) * 100000); // Latitude minutes is multplied by 100000
+                break;
+            }
 
-			case 3:	// Latitude direction
-			{
-				direction = *gps_item;
+            case 3:    // Latitude direction
+            {
+                direction = *gps_item;
 
-				// N is represented as a positive value
-				// S is represented as a negative value
-				if (direction == 'S')
-				{
-					data->latitude_.degrees_ *= -1;
-					data->latitude_.minutes_ *= -1;
-				}
+                // N is represented as a positive value
+                // S is represented as a negative value
+                if (direction == 'S')
+                {
+                    data->latitude_.degrees_ *= -1;
+                    data->latitude_.minutes_ *= -1;
+                }
 
-				break;
-			}
+                break;
+            }
 
-			case 4:
-			{
-				double longitude = (atof(gps_item)); // DDMM.MMMMMM
-				data->longitude_.degrees_ = (int32_t)longitude / 100; // First 2 numbers are the longitude degrees
-				data->longitude_.minutes_ = (int32_t)((longitude - data->longitude_.degrees_ * 100) * 100000); // Longitude minutes is multplied by 100000
-				break;
-			}
+            case 4:
+            {
+                double longitude = (atof(gps_item)); // DDMM.MMMMMM
+                data->longitude_.degrees_ = (int32_t)longitude / 100; // First 2 numbers are the longitude degrees
+                data->longitude_.minutes_ = (int32_t)((longitude - data->longitude_.degrees_ * 100) * 100000); // Longitude minutes is multplied by 100000
+                break;
+            }
 
-			case 5: // Longitude direction
-			{
-				direction = *gps_item;
+            case 5: // Longitude direction
+            {
+                direction = *gps_item;
 
-				// E is represented as a positive value
-				// W is represented as a negative value
-				if (direction == 'W')
-				{
-					data->longitude_.degrees_ *= -1;
-					data->longitude_.minutes_ *= -1;
-				}
+                // E is represented as a positive value
+                // W is represented as a negative value
+                if (direction == 'W')
+                {
+                    data->longitude_.degrees_ *= -1;
+                    data->longitude_.minutes_ *= -1;
+                }
 
-				break;
-			}
+                break;
+            }
 
-			case 9:
-			{
-				data->antennaAltitude_.altitude_ = (int32_t)(atof(gps_item) * 10); // Antenna altitude is multiplied by 10
-				break;
-			}
+            case 9:
+            {
+                data->antennaAltitude_.altitude_ = (int32_t)(atof(gps_item) * 10); // Antenna altitude is multiplied by 10
+                break;
+            }
 
-			case 10: // Antenna altitude unit
-			{
-				data->antennaAltitude_.unit_ = *gps_item;
-				break;
-			}
+            case 10: // Antenna altitude unit
+            {
+                data->antennaAltitude_.unit_ = *gps_item;
+                break;
+            }
 
-			case 11:
-			{
-				data->geoidAltitude_.altitude_ = (int32_t)(atof(gps_item) * 10); // Geoid altitude is multiplied by 10
-				break;
-			}
+            case 11:
+            {
+                data->geoidAltitude_.altitude_ = (int32_t)(atof(gps_item) * 10); // Geoid altitude is multiplied by 10
+                break;
+            }
 
-			case 12: // Geoid altitude unit
-			{
-				data->geoidAltitude_.unit_ = *gps_item;
-				break;
-			}
+            case 12: // Geoid altitude unit
+            {
+                data->geoidAltitude_.unit_ = *gps_item;
+                break;
+            }
 
-			default:
-				break;
-			}
-		}
+            default:
+                break;
+            }
+        }
 
-		counter++;
-		gps_item = &gps_item[item_len + 1];
-	} while (done == 0);
+        counter++;
+        gps_item = &gps_item[item_len + 1];
+    } while (done == 0);
 
-	// Subtract geoid altitude from antenna altitude to get Height Above Ellipsoid (HAE)
-	data->totalAltitude_.altitude_ = data->antennaAltitude_.altitude_ - data->geoidAltitude_.altitude_;
-	data->totalAltitude_.unit_ = data->antennaAltitude_.unit_;
+    // Subtract geoid altitude from antenna altitude to get Height Above Ellipsoid (HAE)
+    data->totalAltitude_.altitude_ = data->antennaAltitude_.altitude_ - data->geoidAltitude_.altitude_;
+    data->totalAltitude_.unit_ = data->antennaAltitude_.unit_;
 
-	memset(gpsTaskRxBuffer, 0, GPS_TASK_RX_BUFFER_SIZE);
-	gpsDataMutex.Unlock();
+    memset(gpsTaskRxBuffer, 0, GPS_TASK_RX_BUFFER_SIZE);
+    gpsDataMutex.Unlock();
 }
