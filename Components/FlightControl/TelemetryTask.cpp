38dd459c8a167cf9e8865d2a3b0db7307f0f0cf1
Christopher Chan <cjchan.chris@gmail.com>
2023-05-20 14:09:25 -0600

    Refactor

--- Components/FlightControl/TelemetryTask.cpp
+++ Components/FlightControl/TelemetryTask.cpp
@@ -85,11 +85,11 @@ void TelemetryTask::HandleCommand(Command& cm)
 void TelemetryTask::RunLogSequence()
 {
 	// Barometer
-    Command cmd(REQUEST_COMMAND, BARO_REQUEST_NEW_SAMPLE);
-    BarometerTask::Inst().GetEventQueue()->Send(cmd);
+    BarometerTask::Inst().SendCommand(Command(REQUEST_COMMAND, (uint16_t)BARO_REQUEST_NEW_SAMPLE));
     BarometerTask::Inst().SendCommand(Command(REQUEST_COMMAND, (uint16_t)BARO_REQUEST_TRANSMIT));
 
     // IMU
+    IMUTask::Inst().SendCommand(Command(REQUEST_COMMAND, (uint16_t)IMU_REQUEST_NEW_SAMPLE));
     IMUTask::Inst().SendCommand(Command(REQUEST_COMMAND, (uint16_t)IMU_REQUEST_TRANSMIT));
 
     // Flight State
