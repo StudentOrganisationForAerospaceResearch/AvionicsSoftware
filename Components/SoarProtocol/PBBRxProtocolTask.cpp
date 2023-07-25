4c3cfd1a10ec2da615ae74580677a42a61e35448
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-07-24 22:23:20 -0600

    commented out line to make console easily readable

--- Components/SoarProtocol/PBBRxProtocolTask.cpp
+++ Components/SoarProtocol/PBBRxProtocolTask.cpp
@@ -89,7 +89,8 @@ void PBBRxProtocolTask::HandleProtobufTelemetryMessage(EmbeddedProto::ReadBuffer
     }
 
     // Copy the message to the read buffer
-	SOAR_PRINT("PROTO-INFO: Received PBB Rx Telemetry Message\n");
+    // TODO : Uncomment this line later
+//	SOAR_PRINT("PROTO-INFO: Received PBB Rx Telemetry Message\n");
 
     EmbeddedProto::WriteBufferFixedSize<DEFAULT_PROTOCOL_WRITE_BUFFER_SIZE> writeBuffer;
     msg.serialize(writeBuffer);
