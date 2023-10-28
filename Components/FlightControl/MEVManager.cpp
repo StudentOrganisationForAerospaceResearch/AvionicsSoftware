6e45ce6cbc36eb36d659221d3f50294fda46c24a
Jesse Gerbrandt <jgerbrandt@live.com>
2023-10-28 12:59:27 -0600

    trying again

--- Components/FlightControl/MEVManager.cpp
+++ Components/FlightControl/MEVManager.cpp
@@ -1,8 +1,8 @@
 #include "MEVManager.hpp"
-#include "PBBRxProtocolTask.hpp"
-#include "CommandMessage.hpp"
+                #include "PBBRxProtocolTask.hpp"
+  #include "CommandMessage.hpp"
 
-MEVManager::MEVState MEVManager::shouldMevBeOpen = INDETERMINATE;
+MEVManager::MEVState MEVManager::shouldMevBeOpen = INDETERMINATE    ;
 
 void MEVManager::OpenMEV() {
     shouldMevBeOpen = OPEN;
@@ -11,11 +11,11 @@ void MEVManager::OpenMEV() {
 
 void MEVManager::CloseMEV() {
     shouldMevBeOpen = CLOSE;
-    PBBRxProtocolTask::SendPBBCommand(Proto::PBBCommand::Command::PBB_CLOSE_MEV);
+    PBBRxProtocolTask::SendPBBCommand(Proto::PBBCommand::Command::PBB_CLOSE_MEV );
 }
 
 void MEVManager::HandleMEVTelemetry(Proto::TelemetryMessage& msg) {
-    if (shouldMevBeOpen == INDETERMINATE || !msg.has_mevstate()) {
+    if (     shouldMevBeOpen == INDETERMINATE || !msg.has_mevstate()) {
         // Do nothing
         return;
     }
