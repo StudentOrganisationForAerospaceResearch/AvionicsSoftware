bef2ca2605fc337ef62acc73f5d112ca5ae8e651
Andrey D <44914169+AndreyDiDev@users.noreply.github.com>
2024-03-24 22:08:12 -0600

    Update SPIDriver.cpp
    setting up

--- Components/Communication/SPIDriver.cpp
+++ Components/Communication/SPIDriver.cpp
@@ -13,11 +13,66 @@
 #include "main_avionics.hpp"
 
 // Declare the global SPI driver objects
-namespace Driver {
+namespace SPIDriver {
 //    UARTDriver uart1(USART1);
-//    UARTDriver uart2(USART2);
-//    UARTDriver uart3(USART3);
-//	UARTDriver uart5(UART5);
+//	  SPIDriver spi(SPI);
+
+}
+
+/**
+ * @brief Initializes SPI driver for the specified ADC
+*/
+void SPIDriver::InitTask(SPI_HandleTypeDef *hspi, Command& cm)
+{
+		// // mcp github start - commands need to be changed
+		uint8_t cmd[4] = {0,0,0,0};
+
+		// 8-bit CONFIG registers
+		cmd[0]  = MCP3561_CONFIG0_WRITE;
+		cmd[1]  = MCP3561_USERCONF_REG0;
+		_MCP3561_write(hspi, cmd, 2);
+
+		cmd[0]  = MCP3561_CONFIG1_WRITE;
+		cmd[1]  = MCP3561_USERCONF_REG1;
+		_MCP3561_write(hspi, cmd, 2);
+
+		cmd[0]  = MCP3561_CONFIG2_WRITE;
+		cmd[1]  = MCP3561_USERCONF_REG2;
+		cmd[1] += 3; // last two bits must always be '11'
+		_MCP3561_write(hspi, cmd, 2);
+
+		cmd[0]  = MCP3561_CONFIG3_WRITE;
+		cmd[1]  = MCP3561_USERCONF_REG3;
+		_MCP3561_write(hspi, cmd, 2);
+
+		cmd[0]  = MCP3561_IRQ_WRITE;
+		cmd[1]  = MCP3561_USERCONF_IRQ_REG;
+		_MCP3561_write(hspi, cmd, 2);
+
+		// 24-bit CONFIG registers
+
+		// configure SCAN mode to automatically cycle through channels
+		// only available for MCP3562 and MCP3564, and only for certain input combinations
+		// @see Datasheet Table 5-14 on p. 54
+		#ifdef MCP3561_USERCONF_SCAN_ENABLE
+			uint32_t reg_val;
+			reg_val = MCP3561_USERCONF_SCAN_REG;
+			cmd[0] = MCP3561_SCAN_WRITE;
+			cmd[1] = (uint8_t)((reg_val >> 16) & 0xff);
+			cmd[2] = (uint8_t)((reg_val >>  8) & 0xff);
+			cmd[3] = (uint8_t)((reg_val)       & 0xff);
+			_MCP3561_write(hspi, cmd, 4);
+
+			reg_val = MCP3561_USERCONF_TIMER_VAL;
+			cmd[0] = MCP3561_TIMER_WRITE;
+			cmd[1] = (uint8_t)((reg_val >> 16) & 0xff);
+			cmd[2] = (uint8_t)((reg_val >>  8) & 0xff);
+			cmd[3] = (uint8_t)((reg_val)       & 0xff);
+			_MCP3561_write(hspi, cmd, 4);
+		#endif
+
+		// mcp github end
+
 }
 
 /**
@@ -26,7 +81,7 @@ namespace Driver {
  * @param len The length of the data to transmit
  * @return True if the transmission was successful, false otherwise
  */
-bool UARTDriver::Transmit(uint8_t* data, uint16_t len)
+bool SPIDriver::WriteADC(int channel)
 {
 //	// Loop through and transmit each byte via. polling
 //	for (uint16_t i = 0; i < len; i++) {
@@ -48,7 +103,7 @@ bool UARTDriver::Transmit(uint8_t* data, uint16_t len)
 * @param receiver
 * @return TRUE if interrupt was successfully enabled, FALSE otherwise
 */
-bool UARTDriver::ReceiveIT(uint8_t* charBuf, UARTReceiverBase* receiver)
+bool SPIDriver::ReadADC(int channel)
 {
 //	// Check flags
 //	HandleAndClearRxError();
