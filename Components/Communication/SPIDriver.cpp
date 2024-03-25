0ad92e5f2c151fc25213575eb25a549ff380d90c
Andrey D <44914169+AndreyDiDev@users.noreply.github.com>
2024-03-24 22:16:46 -0600

    Update SPIDriver.cpp
    setup
    added print

--- Components/Communication/SPIDriver.cpp
+++ Components/Communication/SPIDriver.cpp
@@ -120,3 +120,58 @@ bool SPIDriver::ReadADC(int channel)
 
 	return true;
 }
+
+// start of github
+/**
+ * @brief prints the configuration registers content
+ */
+void MCP3561_PrintRegisters(SPI_HandleTypeDef *hspi){
+	uint8_t reg8 = 0;
+	uint8_t cmd [5] = {0,0,0,0,0};
+
+	cmd[0] = MCP3561_CONFIG0_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("CONF0: %02x\n", reg8);
+
+	cmd[0] = MCP3561_CONFIG1_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("CONF1: %02x\n", reg8);
+
+	cmd[0] = MCP3561_CONFIG2_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("CONF2: %02x\n", reg8);
+
+	cmd[0] = MCP3561_CONFIG3_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("CONF3: %02x\n", reg8);
+
+	cmd[0] = MCP3561_IRQ_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("IRQ  : %02x\n", reg8);
+
+	cmd[0] = MCP3561_MUX_SREAD;
+	reg8 = _MCP3561_sread(hspi, cmd);
+	printf("MUX  : %02x\n", reg8);
+
+	cmd[0] = MCP3561_SCAN_SREAD;
+	uint8_t resp [5] = {0,0,0,0,0};
+
+	HAL_GPIO_WritePin(MCP3561_CHIP_SELECT_GPIO_Port, MCP3561_CHIP_SELECT_GPIO_Pin, GPIO_PIN_RESET);
+	HAL_SPI_TransmitReceive(hspi, cmd, resp, 4, MCP3561_HAL_TIMEOUT);
+	HAL_GPIO_WritePin(MCP3561_CHIP_SELECT_GPIO_Port, MCP3561_CHIP_SELECT_GPIO_Pin, GPIO_PIN_SET);
+
+	printf("SCAN : %02x %02x %02x\n", resp[1], resp[2], resp[3]);
+
+	cmd[0] = MCP3561_TIMER_SREAD;
+
+	HAL_GPIO_WritePin(MCP3561_CHIP_SELECT_GPIO_Port, MCP3561_CHIP_SELECT_GPIO_Pin, GPIO_PIN_RESET);
+	HAL_SPI_TransmitReceive(hspi, cmd, resp, 4, MCP3561_HAL_TIMEOUT);
+	HAL_GPIO_WritePin(MCP3561_CHIP_SELECT_GPIO_Port, MCP3561_CHIP_SELECT_GPIO_Pin, GPIO_PIN_SET);
+
+	printf("TIMER: %02x %02x %02x\n", resp[1], resp[2], resp[3]);
+
+	/* @todo all the remaining registers */
+
+
+}
+// end of github
